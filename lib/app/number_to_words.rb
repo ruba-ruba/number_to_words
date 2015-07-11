feb72ee7f29e257a6ea837316a40ef3fe427e89c
ruba_ruba <gunit1991@ukr.net>
2015-07-11 15:58:21 +0300

    Write logic to make tests pass; cover scenario when user provide not a number

--- lib/app/number_to_words.rb
+++ lib/app/number_to_words.rb
@@ -1,4 +1,38 @@
 class NumberToWords
+  NUMBERS_TO_WORDS_MAPPING = {
+      1000000 => "million",
+      1000 => "thousand",
+      100 => "hundred",
+      90 => "ninety",
+      80 => "eighty",
+      70 => "seventy",
+      60 => "sixty",
+      50 => "fifty",
+      40 => "forty",
+      30 => "thirty",
+      20 => "twenty",
+      19 =>"nineteen",
+      18 =>"eighteen",
+      17 =>"seventeen",
+      16 =>"sixteen",
+      15 =>"fifteen",
+      14 =>"fourteen",
+      13 =>"thirteen",
+      12 =>"twelve",
+      11 => "eleven",
+      10 => "ten",
+      9 => "nine",
+      8 => "eight",
+      7 => "seven",
+      6 => "six",
+      5 => "five",
+      4 => "four",
+      3 => "three",
+      2 => "two",
+      1 => "one",
+      0 => "zero"
+    }
+
   attr_reader :arg
 
   def initialize(arg, options = {})
@@ -9,12 +43,26 @@ class NumberToWords
     unless /^\d{1,}$/.match arg.to_s
       return 'Not a Number'
     end
-    result
+    result(arg)
   end
 
   private
 
-  def result
-    arg
+  def result(number)
+    number = number.to_i
+    str = ""
+    if NUMBERS_TO_WORDS_MAPPING.has_key? number
+      NUMBERS_TO_WORDS_MAPPING[number]
+    else
+      NUMBERS_TO_WORDS_MAPPING.each do |num, name|
+        if number < 100 && number/num > 0
+          return str + "#{name}" if number%num == 0
+          return str + "#{name} " + result(number%num)
+        elsif number/num > 0
+          return str + result(number/num) + " #{name} " + result(number%num)
+        end
+      end
+    end
   end
+
 end
--- spec/number_to_words_spec.rb
+++ spec/number_to_words_spec.rb
@@ -1,13 +1,17 @@
 RSpec.describe NumberToWords do
 
   describe "methods" do
-    numbers = [0, 1, 12, 122, 4050]
-    results = ['zero', 'one', 'twelve', 'one hundred twenty two', 'four thousand fifty']
+    numbers = [0, 1, 7, 12, 122, 4050]
+    results = ['zero', 'one', 'seven', 'twelve', 'one hundred twenty two', 'four thousand fifty']
 
     numbers.each_with_index do |number, index|
       it "convert #{number} to words" do
         expect(NumberToWords.new(number).translate).to eq results[index]
       end
+    end
+
+    it 'says if Not a Number' do
+      expect(NumberToWords.new('Ukraine').translate).to eq 'Not a Number'
     end
   end
 
